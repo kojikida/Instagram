a35e43a587d88827e20474de8ec61f1c80aa00a7
kojikida <koji.in.kiribati@gmail.com>
2020-04-05 06:55:24 +0900

    .gitignore、Podfile、Podfile.lockの追加

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by Koji Kida on 2020/04/04.
+//  Copyright © 2020 Koji Kida. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/SetingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SetingViewController.swift
+//  Instagram
+//
+//  Created by Koji Kida on 2020/04/04.
+//  Copyright © 2020 Koji Kida. All rights reserved.
+//
+
+import UIKit
+
+class SetingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
